398bdee84fa2971f3ed067a77f0724cd4e817f3e
Pavel-Tymoshchuk <38438426+Pavel-Tymoshchuk@users.noreply.github.com>
2020-08-12 11:39:51 +0300

    12.08(11:40)

---
+++ js/index.js
@@ -0,0 +1,92 @@
+Vue.use(VueLazyload)
+
+const app = new Vue({
+    el: "#app",
+    data: {
+        category: null,
+        filterHeaderCategory: 0,
+        banner: null,
+        bannersAmount: 2,
+        currentIndex: 0
+    },
+    methods: {
+        prevSlide: function(){
+            if(this.currentIndex >= 1)
+                this.currentIndex--;
+            else
+                this.currentIndex = this.bannersAmount - 1;
+        },
+        nextSlide: function(){
+            if(this.currentIndex < this.bannersAmount - 1)
+                this.currentIndex++;
+            else
+                this.currentIndex = 0;
+        }
+    },
+    mounted() {
+        this.category = [
+            {
+                name: "Ноутбуки и компьютеры",
+                info: [{name: 'Ноутбуки', slug: 'Ноутбуки'},{name: 'Видеокарты', slug: 'Видеокарты'},{name: 'Мониторы', slug: 'Мониторы'}]
+            },
+            {
+                name: "Детские товары",
+                info: [{name: 'Ноутбуки', slug: 'Ноутбуки'},{name: 'Видеокарты', slug: 'Видеокарты'},{name: 'Мониторы', slug: 'Мониторы'},{name: 'Детские товары', slug: 'Детские товары'}]
+            },
+            {
+                name: "Бытовая техника",
+                info: [{name: 'Ноутбуки', slug: 'Ноутбуки'},{name: 'Видеокарты', slug: 'Видеокарты'},{name: 'Мониторы', slug: 'Мониторы'},{name: 'Бытовая техника', slug: 'Бытовая техника'}]
+            },
+            {
+                name: "Спорт",
+                info: [{name: 'Ноутбуки', slug: 'Ноутбуки'},{name: 'Видеокарты', slug: 'Видеокарты'},{name: 'Мониторы', slug: 'Мониторы'},{name: 'Спорт', slug: 'Спорт'}]
+            },
+        ]
+        
+        this.banner = [
+            {
+                type: "sale",
+                caption: "Выгодное предложение",
+                sub: "Дополнительное описание"
+            },
+            {
+                type: "top",
+                caption: "Выгодное предложение",
+                sub: "Дополнительное описание"
+            }
+        ]
+        
+        var sliderAutoStart;
+        const component = this;
+        setTimeout(function(){
+            let slider = document.querySelector('.main-banner');
+            if(window.innerWidth > 1160){
+                slider.addEventListener('mouseover', function(){
+                    clearInterval(sliderAutoStart);
+                });
+                
+                    
+                slider.addEventListener('mouseleave', () => {
+                    sliderAutoStart = setInterval(component.nextSlide, 5000);
+                });
+                
+                sliderAutoStart = setInterval(component.nextSlide, 5000);
+            }
+            
+            var startPointX;
+            slider.addEventListener("touchstart", function(event) {
+                startPointX = event.changedTouches[0].screenX;
+                clearInterval(sliderAutoStart);
+            }, {passive: true});
+            slider.addEventListener("touchend", function(event){
+                var endPointX = event.changedTouches[0].screenX;
+                
+                if(startPointX - endPointX > 40) {
+                    component.nextSlide();
+                } else if(endPointX - startPointX > 40) {
+                    component.prevSlide();
+                }
+            }, {passive: true});
+        },0)
+    }
+});
---
+++ js/main.js
@@ -0,0 +1,106 @@
+document.addEventListener("DOMContentLoaded", function(){
+    var dropList = document.querySelectorAll('.js-drop-item');
+
+    document.addEventListener('click', function(e){
+        let element = e.target;
+        
+        if(element.closest('.js-drop-button')){
+            let isActive = element.closest('.js-drop-item').classList.contains('active')? true: false;
+            
+            dropList.forEach(item => {item.classList.remove('active')});
+            
+            if(isActive)
+                element.closest('.js-drop-item').classList.remove('active');
+            else
+                element.closest('.js-drop-item').classList.add('active');
+        }
+        
+        if(element.closest('.js-drop-contains')){
+            let dropList = element.closest('.js-drop-item');
+            let dropItems = dropList.querySelectorAll('.js-drop-contains');
+            
+            dropItems.forEach(item => {item.classList.remove('active')});
+            element.closest('.js-drop-contains').classList.add('active');
+            let innerContent = element.closest('.js-drop-contains').querySelector('.text').innerHTML;
+            let dropInput = dropList.querySelector('.js-drop-input');
+            
+            if(dropInput) {
+                dropInput.value = innerContent;
+            }
+            
+            // close dropdown
+            dropList.classList.remove('active');
+        }
+    });
+    
+    document.querySelector('body').addEventListener('click', function(event){
+        
+        let dropItem = event.target.closest('.js-drop-item');
+        
+        if(!dropItem) {
+            document.querySelectorAll('.js-drop-item').forEach(function(item){
+                item.classList.remove('active');
+            }); 
+        }
+        if(dropItem) {
+            if(!dropItem.classList.contains("active")) {
+                document.querySelectorAll('.js-drop-item').forEach(function(item){
+                    item.classList.remove('active');
+                });
+            }
+        }
+        
+    });
+    
+    // Popup
+        
+    let mainButton = document.querySelectorAll('.js-button');
+    let overlay = document.querySelector('.overlay');
+    let htmlOverflow = document.querySelector('html');
+    
+    for(var i = 0; mainButton.length > i; i++) {
+        if(mainButton[i] !== null) {
+            
+            mainButton[i].addEventListener('click', function(){
+                let getData = this.getAttribute('data-target');
+                let popupActive = document.querySelector('.popup.active');
+                let popup = document.querySelector('.popup[data-target = ' + getData + ']');
+                popup.classList.add('active');
+                overlay.classList.add('active');
+                htmlOverflow.classList.add('overflow');
+                
+                if(popupActive) {
+                    popupActive.classList.remove('active');
+                }
+            });
+        }
+    }
+    
+    document.addEventListener('click', function(e){
+        let elem = e.target;
+        
+        if(elem.closest('.js-close')){
+            let popupActive = document.querySelector('.popup.active');
+            if(!popupActive.classList.contains("js-gallery")) {
+                popupActive.classList.remove('active');
+                overlay.classList.remove('active');
+                htmlOverflow.classList.remove('overflow');
+            }else {
+                popupActive.classList.remove('active');
+                overlay.classList.remove('active');
+                setTimeout(function(){
+                    htmlOverflow.classList.remove('overflow');
+                },600);
+            }
+        }
+    });
+
+    overlay.addEventListener('click', function(){
+        let popupActive = document.querySelector('.popup.active');
+        popupActive.classList.remove('active');
+        overlay.classList.remove('active');
+        htmlOverflow.classList.remove('overflow');
+    });
+    
+    // Popup
+});
