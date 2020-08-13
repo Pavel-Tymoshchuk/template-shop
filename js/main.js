9d36f3229be9c41f26a9ab3876d82febfb7e9f66
Pavel-Tymoshchuk <38438426+Pavel-Tymoshchuk@users.noreply.github.com>
2020-08-13 17:47:34 +0300

    13.08(18:00)

--- js/main.js
+++ js/main.js
@@ -134,4 +134,80 @@ document.addEventListener("DOMContentLoaded", function(){
     });
     
     // /More info
+    
+    // Slider one elem move
+    
+    // Main slider 
+    
+    document.addEventListener('click', function(e){
+        let item = e.target;
+        
+        if(item.closest('.js-oneMove-arrow-prev')) {
+            let activeElem = item.closest('.js-oneMove-wrapper').querySelector('.js-oneMove-item.show');
+            let allItems = item.closest('.js-oneMove-wrapper').querySelectorAll('.js-oneMove-item');
+            
+            if(activeElem.previousElementSibling == null) {
+                activeElem.classList.remove('show');
+                allItems[allItems.length - 1].classList.add('show');
+                return;
+            }
+            
+            activeElem.previousElementSibling.classList.add('show');
+            activeElem.classList.remove('show');
+        }
+        
+        if(item.closest('.js-oneMove-arrow-next')) {
+            let activeElem = item.closest('.js-oneMove-wrapper').querySelector('.js-oneMove-item.show');
+            let allItems = item.closest('.js-oneMove-wrapper').querySelectorAll('.js-oneMove-item');
+            
+            if(activeElem.nextElementSibling == null) {
+                activeElem.classList.remove('show');
+                allItems[0].classList.add('show');
+                return;
+            }
+            
+            activeElem.nextElementSibling.classList.add('show');
+            activeElem.classList.remove('show');
+        }
+    });
+    
+    // /Main slider
+    
+    // Mask tel
+    
+    [].forEach.call( document.querySelectorAll('input[type="tel"]'), function(input) {
+        var keyCode;
+        function mask(event) {
+            event.keyCode && (keyCode = event.keyCode);
+            var pos = this.selectionStart;
+            if (pos < 3) event.preventDefault();
+            var matrix = "+38(___)-___-____",
+                i = 0,
+                def = matrix.replace(/\D/g, ""),
+                val = this.value.replace(/\D/g, ""),
+                new_value = matrix.replace(/[_\d]/g, function(a) {
+                    return i < val.length ? val.charAt(i++) || def.charAt(i) : a
+                });
+            i = new_value.indexOf("_");
+            if (i != -1) {
+                i < 5 && (i = 3);
+                new_value = new_value.slice(0, i)
+            }
+            var reg = matrix.substr(0, this.value.length).replace(/_+/g,
+                function(a) {
+                    return "\\d{1," + a.length + "}"
+                }).replace(/[+()]/g, "\\$&");
+            reg = new RegExp("^" + reg + "$");
+            if (!reg.test(this.value) || this.value.length < 5 || keyCode > 47 && keyCode < 58) this.value = new_value;
+            if (event.type == "blur" && this.value.length < 5)  this.value = ""
+        }
+    
+        input.addEventListener("input", mask, false);
+        input.addEventListener("focus", mask, false);
+        input.addEventListener("blur", mask, false);
+        input.addEventListener("keydown", mask, false)
+    
+    });
+        
+    // Mask tel
 });
